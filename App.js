736aaa55e7c70e447dd8f48ab67ddf2c12c1b77c
Anish Pokharkar <anishsp3@illinois.edu>
2022-09-29 17:41:27 -0500

    started log in page

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ImageBackground, TouchableHighlight, ScrollView, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+let dh = Dimensions.get('window').height;
+let dw = Dimensions.get('window').width;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerLogIn}>
+      <View style={styles.LogInSections}>
+        <Text style={styles.LogInTopText}>
+          Log In
+        </Text>
+      </View>
+      <View style={styles.LogInSections}>
+        <View style={styles.LogInInputAndInfo}>
+          
+        </View>
+        <View style={styles.LogInInputAndInfo}>
+          
+        </View>
+        <View style={styles.LogInInputAndInfo}>
+          
+        </View>
+      </View>
+      <View style={styles.LogInSections}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  containerLogIn: {
+    height: dh,
+    width: dw,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'block',
+  },
+  LogInSections: {
+    height: dh/3,
+    width: dw,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  LogInTopText: {
+    textAlign: 'center',
+    font: 'Avenir',
+    fontSize: dh/15,
+    fontWeight: 'bold',
+    color: '#FDCA8C'
+  },
+  LogInInputAndInfo: {
+    height: dh/12,
+    width: dw,
+    paddingVertical: dh/72,
+    flexDirection: 'row',
+  },
+  LogInInfo: {
+    height: dh/12,
+    width: dw/3,
+    paddingVertical: dh/72,
+  },
+  LogInInputs: {
+    height: dh/12,
+    width: dw/6,
+    paddingVertical: dh/72,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'gray',
   },
 });
